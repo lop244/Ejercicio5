baa80b963ce952404c0fb5426fccc499dd04810d
lop244 <lop24415@uvg.edu.gt>
2025-10-06 19:26:46 -0600

    comentarios sobre la clase

--- LupoOs/Main.java
+++ LupoOs/Main.java
@@ -2,3 +2,7 @@ package controlador;
 
 import modelo.*;
 import vista.Vista;
+/**
+ * Clase principal que actúa como Controlador en el patrón MVC.
+ * Orquesta la interacción entre el Modelo (SistemaOperativo) y la Vista.
+ */
