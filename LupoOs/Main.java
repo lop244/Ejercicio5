c524faa9ce6772416b3595218ed2b540a63fd818
lop244 <lop24415@uvg.edu.gt>
2025-10-06 19:27:15 -0600

    definimos la clase y sus metodos

--- LupoOs/Main.java
+++ LupoOs/Main.java
@@ -6,3 +6,6 @@ import vista.Vista;
  * Clase principal que actúa como Controlador en el patrón MVC.
  * Orquesta la interacción entre el Modelo (SistemaOperativo) y la Vista.
  */
+public class Main {
+    private SistemaOperativo modelo;
+    private Vista vista;
