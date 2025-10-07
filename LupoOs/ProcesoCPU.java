3b381e19868cf60d33325b006745c033763124ff
lop244 <lop24415@uvg.edu.gt>
2025-10-06 19:54:59 -0600

    hereda proceso y se define la super clase

--- LupoOs/ProcesoCPU.java
+++ LupoOs/ProcesoCPU.java
@@ -1,5 +1,8 @@
 package LupoOs.modelo;
 
-public class ProcesoCPU {
+public class ProcesoCPU extends Proceso {
+    public ProcesoCPU(int pid, String nombre) {
+        super(pid, nombre);
+    }
+ 
     
-}
