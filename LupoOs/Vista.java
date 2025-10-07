5954b92889101a33b9a72890ed2d1c20b379e98e
lop244 <lop24415@uvg.edu.gt>
2025-10-06 20:18:57 -0600

    comentario

--- LupoOs/Vista.java
+++ LupoOs/Vista.java
@@ -2,6 +2,7 @@ package LupoOs.vista;
 
 import java.util.Scanner;
 
+// La clase que el usuario verá e interactuará con, es decir entrada y salida de datos
 
 public class Vista {
     private Scanner scanner;
