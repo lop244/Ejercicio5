54687d8a07d4dc0e77cc14300c00a622914d7eda
lop244 <lop24415@uvg.edu.gt>
2025-10-06 19:56:46 -0600

    agregue comentarios y override, tambien simulador de tarea

--- LupoOs/ProcesoCPU.java
+++ LupoOs/ProcesoCPU.java
@@ -1,8 +1,18 @@
 package LupoOs.modelo;
 
+// Representa un proceso intensivo en CPU, como la compilación de código o el renderizado de gráficos.
+
+
 public class ProcesoCPU extends Proceso {
     public ProcesoCPU(int pid, String nombre) {
         super(pid, nombre);
     }
- 
+
+    @Override
+    public String ejecutar() {
+        // Simula una tarea de cálculo intensivo
+        return "[CPU] PID " + getPid() + " (" + getNombre() + "): Realizando cálculos complejos. Finalizado.";
+    }
+}
+
     
