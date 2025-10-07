3b04ab9de2ec91ea2b0ba1f0b47720a32d9c5c43
lop244 <lop24415@uvg.edu.gt>
2025-10-06 20:06:38 -0600

    override con su return y su ejecutar

--- LupoOs/ProcesoDaemon.java
+++ LupoOs/ProcesoDaemon.java
@@ -5,3 +5,8 @@ public class ProcesoDaemon extends Proceso {
     public ProcesoDaemon(int pid, String nombre) {
         super(pid, nombre);
 }
+
+@Override
+public string ejecutar() {
+    return "[DAEMON] PID " + getPid() + " (" + getNombre() + "): Servicio de monitoreo activo y en ejecución.";
+}
