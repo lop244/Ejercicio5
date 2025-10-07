24020701d3fc020afd4a669b0491fc4a515b3387
lop244 <lop24415@uvg.edu.gt>
2025-10-06 20:01:46 -0600

    override y agregamos la funciond e ejecutar

--- LupoOs/ProcesoIO.java
+++ LupoOs/ProcesoIO.java
@@ -4,3 +4,8 @@ public class ProcesoIO extends Proceso {
     public ProcesoIO(int pid, String nombre) {
         super(pid, nombre);
 }
+@Override
+public String ejecutar() {
+        return "[I/O] PID " + getPid() + " (" + getNombre() + "): Esperando respuesta del disco duro. Completado.";
+    }
+}
